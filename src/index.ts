e80837c63128f5647462a8fbebe60e60fef1a51d
Marcus Nebel <marcusnebel09@gmail.com>
2026-01-16 20:05:22 +0100

    Refactor stream handling and clean up utilities
    Replaces the stream.ts script with a reusable getStreamURL function and updates index.ts to use it for the /stream route. Removes unused utility scripts (initialize.ts, check_jimce_connection.ts), and moves check_dependencies.ts and tools.ts into a dev subdirectory for better organization.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // Import the framework and instantiate it
 import Fastify from 'fastify'
+import streamYoutubeURL from './components/getStreamURL'
 
 const fastify = Fastify({
     logger: true
@@ -8,36 +9,18 @@ const fastify = Fastify({
 // Declare a route
 fastify.get('/info', async function download() {
     // /info
-    console.log('Request für /info erhalten') // {
-    return { hello: '/info' } //   "name": "jimce-music/sound-provider-youtube",
-}) //   "license": "MIT",
-
-//   "provider-type": "youtube",
-//   "expected-sound-quality": "medium",
-
-//   "best-for": {
-//     "streaming": true,
-//     "downloads": false
-//   },
-
-//   "capabilities": {
-//     "streaming": [true, "/stream"],
-//     "downloads": [true, "/download"]
-//   },
-
-//   "accepted-identifiers": ["youtube:id", "youtube:url"]
-// }
+    console.log('Request für /info erhalten')
+})
 
 fastify.get('/request-play', async function download() {
     // GET /request-play?identifier=dQw4w9WgXcQ&just-download=false&save-while-streaming=true&downloaded-callback=http://jimce-server:8080/api/downlaoded-callback/67as0fhufuiashiu
     console.log('Request für /request-play erhalten')
-    return { hello: '/request-play' }
 })
 
 fastify.get('/stream', async function download() {
     // GET /stream?id=[uuid]
     console.log('Request für /stream erhalten')
-    return { hello: '/stream' }
+    await streamYoutubeURL()
 })
 
 fastify.get('/download', async function download(request, reply) {
--- src/utils/initialize.ts
+++
@@ -1,8 +0,0 @@
-import { checkDependencies } from './check_dependencies'
-import { checkJimceServerConnection } from './check_jimce_connection'
-
-console.log('Dependencies check started...')
-await checkDependencies()
-
-console.log('Ping Jimce Server...')
-await checkJimceServerConnection()
